dd70e522c1301856e3bd59025d4308d7a4555672
Markas Korotkovas <mkorotkovas@yahoo.com>
2025-11-24 22:53:43 -0700

    first

---
+++ src/main/java/net/thekttn/kttn_mail_api/KttnMailApiApplication.java
@@ -0,0 +1,13 @@
+package net.thekttn.kttn_mail_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KttnMailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KttnMailApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/thekttn/kttn_mail_api/KttnMailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thekttn.kttn_mail_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KttnMailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
