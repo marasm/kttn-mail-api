4b1c2cfb99ba6db06358e049ae19a94afcbd3588
Markas Korotkovas <mkorotkovas@yahoo.com>
2025-11-28 22:30:50 +0000

    added encrypt/decrypt methods + test

--- src/main/java/net/thekttn/kttnmail/util/CryptoUtil.java
+++ src/main/java/net/thekttn/kttnmail/util/CryptoUtil.java
@@ -7,13 +7,59 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.Multipart;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
 
-import org.apache.commons.codec.digest.DigestUtils;;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;;
+
+@Component
 public class CryptoUtil
 {
+  private static final String ENCRYPTION_ALGORITHM = "AES/GCM/NoPadding";
+  private static final String SECRET_KEY_ALGORITHM = "PBKDF2WithHmacSHA256";
+  private static final String KEY_MATERIAL_ALGORITHM = "AES";
+  private static final int ITERATION_COUNT = 65536;
+  private static final int KEY_LENGTH = 256;
+  private static final int INIT_VECTOR_LEN = 128;
 
-  public static String hashMessage(Message inMessage) throws MessagingException, IOException
+  @Value("${crypto.key-secret}")
+  private String keySecret;
+  @Value("${crypto.key-salt}")
+  private String keySalt;
+  @Value("${crypto.init-vector}")
+  private String initVector;
+
+
+  private Cipher encryptor;
+  private Cipher decryptor;
+
+  public String encryptString(String inString) throws IllegalBlockSizeException, BadPaddingException, Exception
+  {
+    byte[] cipherText = getEncryptor().doFinal(inString.getBytes());
+    return Base64.getEncoder().encodeToString(cipherText);
+  }
+
+  public String decryptString(String inString)  throws IllegalBlockSizeException, BadPaddingException, Exception
+  {
+    byte[] decodedBytes = Base64.getDecoder().decode(inString);
+    byte[] plainText = getDecryptor().doFinal(decodedBytes);
+    return new String(plainText);
+  }
+
+  public String hashMessage(Message inMessage) throws MessagingException, IOException
   {
     
     StringBuilder sb = new StringBuilder();
@@ -51,6 +97,63 @@ public class CryptoUtil
 
 
     return DigestUtils.sha512Hex(sb.toString());
+  }
+
+
+  private Cipher getEncryptor() throws Exception
+  {
+    if(encryptor == null)
+    {
+      SecretKey secretKey = getSecretKey();
+      GCMParameterSpec ivSpec = getInitVector();
+
+      encryptor = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+      encryptor.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
+    }
+
+    return encryptor;
+  }
+  
+  private Cipher getDecryptor() throws Exception
+  {
+    if(decryptor == null)
+    {
+      SecretKey secretKey = getSecretKey();
+      GCMParameterSpec ivSpec = getInitVector();
+
+      decryptor = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+      decryptor.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+    }
+
+    return decryptor;
+  }
+
+
+  private SecretKey getSecretKey() throws InvalidKeySpecException, NoSuchAlgorithmException
+  {
+    if(keySecret == null || keySalt == null)
+    {
+      throw new IllegalStateException("Key secret or salt not set");
+    }
+
+    SecretKeyFactory factory = SecretKeyFactory.getInstance(SECRET_KEY_ALGORITHM);
+    KeySpec spec = new PBEKeySpec(keySecret.toCharArray(), keySalt.getBytes(), ITERATION_COUNT, KEY_LENGTH);
+    SecretKey secret = new SecretKeySpec(factory.generateSecret(spec)
+        .getEncoded(), KEY_MATERIAL_ALGORITHM);
+    return secret;
+  }
+
+
+  private GCMParameterSpec getInitVector()
+  {
+    if(initVector == null)
+    {
+      throw new IllegalStateException("Initialization vector not set");
+    }
+
+    byte[] ivBytes = Base64.getDecoder().decode(initVector.getBytes());
+
+    return new GCMParameterSpec(INIT_VECTOR_LEN, ivBytes);
   }
 
 }
--- src/test/java/net/thekttn/kttnmail/util/TestCryptoUtil.java
+++ src/test/java/net/thekttn/kttnmail/util/TestCryptoUtil.java
@@ -5,8 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.AddressException;
@@ -16,6 +18,8 @@ import jakarta.mail.internet.MimeMessage;
 public class TestCryptoUtil
 {
   private MimeMessage message;
+  private CryptoUtil util;
+  
   @BeforeEach
   public void setup() throws AddressException, MessagingException
   {
@@ -24,12 +28,36 @@ public class TestCryptoUtil
     message.addRecipient(MimeMessage.RecipientType.TO, new InternetAddress("recipient@example.com"));
     message.setSubject("Test Subject");
     message.setText("This is the body of the email.");
+
+    util = new CryptoUtil();
+    ReflectionTestUtils.setField(util, "keySecret", "testSecret");
+    ReflectionTestUtils.setField(util, "keySalt", "testSalt");
+    ReflectionTestUtils.setField(util, "initVector", "r++nXVdcyxNlv6C1");
+  }
+
+  @AfterEach
+  public void teardown() throws MessagingException
+  {
+    message = null;
+    util = null;
+  }
+
+
+  @Test
+  void testEncryptDecryptString_SUCCESS() throws Exception
+  {
+    String originalString = "This is a test string for encryption.";
+    String encryptedString = util.encryptString(originalString);
+    String decryptedString = util.decryptString(encryptedString);
+
+    assertNotEquals(originalString, encryptedString);
+    assertTrue(decryptedString.equals(originalString));
   }
 
   @Test
   void testHashMessage_SUCCESS() throws MessagingException, IOException
   {
-    String hash = CryptoUtil.hashMessage(message);
+    String hash = util.hashMessage(message);
 
     assertTrue(hash != null && !hash.isEmpty());
     assertTrue(hash.length() == 128); // SHA-512 produces a 128-character hex string
@@ -38,10 +66,10 @@ public class TestCryptoUtil
   @Test
   void testHashMessage_HASHES_DIFFERENT() throws MessagingException, IOException
   {
-    String hash1 = CryptoUtil.hashMessage(message);
+    String hash1 = util.hashMessage(message);
     
     message.setText("This is a modified body of the email.");
-    String hash2 = CryptoUtil.hashMessage(message);
+    String hash2 = util.hashMessage(message);
 
     assertTrue(hash1 != null && !hash1.isEmpty());
     assertTrue(hash1.length() == 128); // SHA-512 produces a 128-character hex string
@@ -53,7 +81,7 @@ public class TestCryptoUtil
   @Test
   void testHashMessage_NULL_MESSAGE() throws MessagingException, IOException
   {
-    String hash = CryptoUtil.hashMessage(null);
+    String hash = util.hashMessage(null);
     assertTrue(hash != null && !hash.isEmpty());
     assertTrue(hash.length() == 128); // SHA-512 produces a 128-character hex string even for an empty string
   }
